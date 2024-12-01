ab433ae7147747edbaf89c6c1efd84426c598ebf
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-12-01 11:55:07 -0800

    asking for suggestions

--- script.js
+++ script.js
@@ -5,12 +5,15 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
+//answer
 console.log("The hands-on projects and practical exercises really helped reinforce my understanding of the concepts");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
+// This statement is asking for suggestions on improving the course
 console.log("What would you change about the course?");
+//answer
 console.log("Adding more live coding sessions and opportunities for interactive discussions would have been beneficial.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
